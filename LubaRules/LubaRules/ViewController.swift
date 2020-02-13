d196d6d8b7917ace6d05c7074b2bf6ba3170b8f1
LubaKaper <liubovkaper@pursuit.com>
2020-02-13 10:57:40 -0500

    added Xcode project

---
+++ LubaRules/LubaRules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LubaRules
+//
+//  Created by Liubov Kaper  on 2/13/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
